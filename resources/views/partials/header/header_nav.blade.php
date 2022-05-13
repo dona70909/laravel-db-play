50a3e50428ee79ef924ba475b32d1fae8663dd74
Donatella7 <campaniellodonatella@gmail.com>
2022-05-14 00:21:00 +0200

    Style header nav

--- resources/views/partials/header/header_nav.blade.php
+++ resources/views/partials/header/header_nav.blade.php
@@ -1,7 +1,8 @@
 <section class="container-fluid">
-    <div class="row">
-        <div class="col-12">
-            here nav
+    <div class="row p-4">
+        <div class="col-12 d-flex align-items-baseline">
+            <h1 class="me-4">The movie DB</h1>
+            <a class="text-dark text-decoration-none" href="{{url('/')}}"><h4>Home</h4></a>
         </div>
     </div>
 </section>
