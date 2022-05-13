5ce097a10406ea015e879e4be1f1415927f3c891
Donatella7 <campaniellodonatella@gmail.com>
2022-05-14 00:17:45 +0200

    Style card

--- resources/views/partials/main/main_cards.blade.php
+++ resources/views/partials/main/main_cards.blade.php
@@ -1,12 +1,12 @@
 <section class="container-fluid">
-    <div class="row d-flex">
+    <div class="row d-flex movie-card-wrapper">
         @foreach ($movies as $movie)
-            <div class="card col-2" style="width: 18rem;">
+            <div class="card col-2">
                 <img class="card-img-top" src="{{$movie->cover}}" alt="Card image cap">
                 <div class="card-body">
-                    <h5 class="card-title"></h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    
+                    <p class="card-text">{{$movie->title}}</p>
+                    <h5 class="card-title">Vote: {{$movie->vote}}</h5>
                 </div>
             </div>
         @endforeach
