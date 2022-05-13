8a88948339533d00b54d3d81edb5b04c13790d69
Donatella7 <campaniellodonatella@gmail.com>
2022-05-13 22:50:23 +0200

    created View Controller and Model + update table

---
+++ database/migrations/2022_05_13_204046_create_movies.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMovies extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('genre');
+            $table->string('language');
+            $table->tinyInteger('vote');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
---
+++ resources/views/guest/home/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('header-content')
+    @include('partials.header.header_nav')
+@endsection
+
+@section('main-content')
+    @include('partials.main.main_cards')
+@endsection
