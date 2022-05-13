cbeee8cafa4ac1832644bacbabd87bb2985bd783
Donatella7 <campaniellodonatella@gmail.com>
2022-05-13 23:09:21 +0200

    done seeder php artisan db:seed IT WORKS

--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -7,6 +7,12 @@ use Faker\Generator as Faker;
 
 $factory->define(Movie::class, function (Faker $faker) {
     return [
-        //
+        
+        'title' => $faker->text,
+        'genre' => $faker->text,
+        'language' => $faker->text,
+        'vote' => $faker->numberBetween(1,5),
+        'cover' => $faker->imageUrl($width = 640, $height = 480),
+        
     ];
 });
