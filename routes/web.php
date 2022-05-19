7e48d4f789a255c9066aeb3298b49d951e915fcf
Donatella7 <campaniellodonatella@gmail.com>
2022-05-19 10:52:41 +0200

    Auth

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::post('store-form','MovieController@insert');
 
 Route::get('/form','MovieController@show',function () {
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
